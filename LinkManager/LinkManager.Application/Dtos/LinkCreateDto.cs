cd82aa4c3875660c3fab02b3a22af2d57a159f02
Kkkiittt <khamidov357@gmail.com>
2025-05-28 15:20:27 +0500

    create dto

--- LinkManager/LinkManager.Application/Dtos/LinkCreateDto.cs
+++ LinkManager/LinkManager.Application/Dtos/LinkCreateDto.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LinkManager.Application.Dtos;
 
-namespace LinkManager.Application.Dtos
+public class LinkCreateDto
 {
-	internal class LinkCreateDto
+	public string LongLink { get; set; }
+	public string? Password { get; set; }
+	public int Lifetime { get; set; }
+
+	public LinkCreateDto(string longLink, string? password, int lifetime)
 	{
+		LongLink = longLink;
+		Password = password;
+		Lifetime = lifetime;
 	}
 }
--- LinkManager/LinkManager.Application/Dtos/LinkUpdateDto.cs
+++ LinkManager/LinkManager.Application/Dtos/LinkUpdateDto.cs
@@ -1,17 +1,11 @@
 ﻿namespace LinkManager.Application.Dtos;
 
-public class LinkUpdateDto
+public class LinkUpdateDto : LinkCreateDto
 {
 	public string ShortLink { get; set; }
-	public string LongLink { get; set; }
-	public string? Password { get; set; }
-	public int Lifetime { get; set; }
 
-	public LinkUpdateDto(string shortLink, string longLink, string? password, int lifetime)
+	public LinkUpdateDto(string shortLink, string longLink, string? password, int lifetime) : base(longLink, password, lifetime)
 	{
 		ShortLink = shortLink;
-		LongLink = longLink;
-		Password = password;
-		Lifetime = lifetime;
 	}
 }
