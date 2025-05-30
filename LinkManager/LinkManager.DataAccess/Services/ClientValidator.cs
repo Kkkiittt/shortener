136cc3331e2ce2a9757857ff7ce53d2a61d5c10b
Kkkiittt <khamidov357@gmail.com>
2025-05-30 14:31:41 +0500

    validator?

---
+++ LinkManager/LinkManager.DataAccess/Services/ClientValidator.cs
@@ -0,0 +1,25 @@
+﻿using LinkManager.Application.Dtos;
+using LinkManager.Application.Interfaces.Services;
+
+using PlanManager.Application.Dtos;
+using PlanManager.Application.Interfaces.Module;
+using PlanManager.Domain.Enums;
+
+namespace LinkManager.DataAccess.Services;
+
+public class ClientValidator : IClientValidator
+{
+	private readonly IPlanManagerModule _plan;
+
+	public ClientValidator(IPlanManagerModule plan)
+	{
+		_plan = plan;
+	}
+
+	public bool Validate(ClientCheckDto dto)
+	{
+		PlanAction action = (PlanAction)Enum.Parse(typeof(PlanAction), dto.Action.ToString());
+		PlanCheckDto planDto = new PlanCheckDto(dto.PlanId, dto.LinkCount, dto.LinkLifetime, action);
+		return _plan.CheckPlan(planDto);
+	}
+}
