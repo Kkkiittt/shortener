75f971701d3d6c2141b8deaa2f696ca23f161bdb
Kkkiittt <khamidov357@gmail.com>
2025-06-09 15:49:30 +0500

    module extensions

---
+++ Authenticator/Authenticator.Api/Services/AuthenticatorModuleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Authenticator.Application.Interfaces.Repositories;
+using Authenticator.Application.Interfaces.Services;
+using Authenticator.Application.Services;
+using Authenticator.DataAccess.Configurations;
+using Authenticator.DataAccess.Contexts;
+using Authenticator.DataAccess.Repositories;
+using Authenticator.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+using Shared.Interfaces;
+using Shared.Services;
+
+namespace Authenticator.Api.Services;
+
+public static class AuthenticatorModuleExtensions
+{
+	public static IServiceCollection AddAuthenticatorModule(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddScoped<IUserManager, UserManager>();
+		services.AddScoped<ITokenGenerator, TokenGenerator>();
+		services.AddScoped<IUserRepository, UserRepository>();
+		services.AddScoped<IEntityTypeConfiguration<User>, UserEntityTypeConfiguration>();
+		services.AddDbContext<UserDbContext>(options =>
+		{
+			var connect = config.GetConnectionString("UserDb");
+			options.UseNpgsql(connect);
+		});
+		return services;
+	}
+}
---
+++ LinkManager/LinkManager.Api/Services/LinkManagerModuleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using LinkManager.Application.Interfaces.Repositories;
+using LinkManager.Application.Interfaces.Services;
+using LinkManager.Application.Services;
+using LinkManager.DataAccess.Configurations;
+using LinkManager.DataAccess.Contexts;
+using LinkManager.DataAccess.Repositories;
+using LinkManager.DataAccess.Services;
+using LinkManager.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+using Shared.Interfaces;
+using Shared.Services;
+
+namespace LinkManager.Api.Services;
+
+public static class LinkManagerModuleExtensions
+{
+	public static IServiceCollection AddLinkManagerModule(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddScoped<ILinkRepository, LinkRepository>();
+		services.AddScoped<IClientValidator, ClientValidator>();
+		services.AddScoped<ILinkService, LinkService>();
+		services.AddScoped<IEntityTypeConfiguration<Link>, LinkEntityTypeConfiguration>();
+		services.AddDbContext<LinkDbContext>((serv, opt) =>
+		{
+			opt.UseNpgsql(config.GetConnectionString("LinkDb"));
+		});
+		return services;
+	}
+}
